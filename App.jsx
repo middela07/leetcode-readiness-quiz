9c193a6123c62161d00876437b216925adf066f7
middela07 <51866569+middela07@users.noreply.github.com>
2025-06-05 17:50:25 -0500

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// File: src/App.jsx
+import React from "react";
+import LeetCodeQuiz from "./components/LeetCodeQuiz";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 py-10">
+      <LeetCodeQuiz />
+    </main>
+  );
+}
+
+export default App;
